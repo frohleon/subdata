d154d1e85d661d1e93500d467bdd18439e0d0d76
Leon Froehling <leonf.1996@googlemail.com>
2024-12-01 21:48:55 +1000

    changed version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setup(
     name='subdata',  # Replace with your package’s name
-    version='0.1.12',
+    version='0.1.13',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[ # List your dependencies here
